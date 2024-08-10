5f2765ae80b67964c53282c94eb817c25434b217
AnirudhHack <anirudhahack123@gmail.com>
2024-08-11 00:53:10 +0530

    add vault logic

--- smartContracts/contracts/Vault.sol
+++ smartContracts/contracts/Vault.sol
@@ -27,6 +27,17 @@ contract WSTETHVault is FlashLoanSimpleReceiverBase, ERC20, IERC4626, Reentrancy
     event PendingGovernance(address indexed governance);
     event GovernanceAccepted(address indexed newGovernance);
 
+    struct RebalanceInput{
+        uint supplyAmount;
+        uint flAmount;
+        uint flAmountWithFee;
+        uint slippagePercent;
+    }
+
+    enum VauitAction{
+        Rebalance
+    }
+
     /**
 	 * @dev constructor
 	 * @notice Intializes state variables of vault.
@@ -82,16 +93,6 @@ contract WSTETHVault is FlashLoanSimpleReceiverBase, ERC20, IERC4626, Reentrancy
         emit Deposit(msg.sender, receiver, returnAmount, shares);
     }
 
-    function executeOperation(
-        address asset,
-        uint256 amount,
-        uint256 premium,
-        address initiator,
-        bytes calldata params
-    ) external override returns (bool) {
-
-    }
-
     /**
 	 * @dev withdraw
 	 * @notice Withdraw function allows user to withdraw there eth amount by burning shares.
@@ -129,6 +130,187 @@ contract WSTETHVault is FlashLoanSimpleReceiverBase, ERC20, IERC4626, Reentrancy
 
         emit Withdraw(msg.sender, receiver, owner, assets, shares);
     }
+
+    function rebalance(uint supplyAmount, uint flAmount, uint flAmountWithFee, uint slippagePercent)private {
+        AaveInterface aave = AaveInterface(aaveProvider.getPool());
+
+        uint slippageAmt = calculateSlippageAmount(supplyAmount, slippagePercent);
+
+        uint returnAmount = uniswapV3Swap(SellInfo(
+            address(WETH9),
+            asset,
+            fee,
+            slippageAmt,
+            flAmount
+        ));
+
+        supplyAmount += returnAmount;
+        // call supply
+        ERC20(asset).approve(address(aave), supplyAmount);
+        aave.supply(asset, supplyAmount, address(this), 0);
+
+        aave.borrow(address(WETH9), flAmountWithFee, 2, 0, address(this));
+    }
+
+    function executeOperation(
+        address assetaddr,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        // require(msg.sender == address(LENDING_POOL), "only lending pool");
+        // require(address(this) == initiator, "Untrusted loan initiator");
+        // AaveInterface aave = AaveInterface(aaveProvider.getPool());
+
+        // // abi.decode(params) to decode params
+        // (uint8 operation, bytes memory data, address receiver, ,) = abi.decode(params, (uint8, bytes, address, uint, uint));
+        // uint24 fee = 3000;
+        // uint slippageAmt = 10;
+        // // deposit and reinvestIdleWeth logic
+        // if(operation == 0 || operation == 4){
+        //     uint supplyAmount;
+        //     if(vaultsLeverage == Leverage.Three) supplyAmount = amounts[0] + (amounts[0]/2);
+        //     else supplyAmount = amounts[0] + amounts[0];
+            
+        //     uint returnAmount = uniswapV3Swap(SellInfo(
+        //         address(WETH9),
+        //         asset,
+        //         fee,
+        //         slippageAmt,
+        //         supplyAmount
+        //     ));
+
+        //     // calculating assets in steth on which share to be calculated and minted (only for operation 1 (deposit))
+        //     if(operation == 0){
+        //         uint inputUserAssets;
+        //         if(vaultsLeverage == Leverage.Three) inputUserAssets = returnAmount / 3;
+        //         else inputUserAssets = returnAmount / 2;
+                
+        //         uint shares;
+        //         require((shares = previewDeposit(inputUserAssets)) != 0, "ZERO_SHARES");
+        //         usersShareToBeMinted[receiver] = shares;
+        //     }
+
+        //     // call supply
+        //     ERC20(asset).approve(address(aave), returnAmount);
+        //     aave.supply(asset, returnAmount, address(this), 0);
+
+        //     aave.borrow(address(WETH9), amounts[0], 2, 0, address(this));
+        // }
+        // //withdraw
+        // else if(operation == 1){
+        //     ( , , , ,uint withdrawAmount) = abi.decode(params, (uint8, bytes, address, uint, uint));
+
+        //     // repay Weth on aave
+        //     WETH9.approve(address(aave), amounts[0]);
+        //     aave.repay(address(WETH9), amounts[0], 2, address(this));
+
+        //     // withdraw stEth from aave
+        //     aave.withdraw(asset, withdrawAmount, address(this));
+
+        //     // swap 
+        //     uint returnAmount = uniswapV3Swap(SellInfo(
+        //         asset,
+        //         address(WETH9),
+        //         fee,
+        //         slippageAmt,
+        //         withdrawAmount
+        //     ));
+        //     uint userReceivable = returnAmount - (amounts[0] + premiums[0]);
+        //     WETH9.withdraw(userReceivable);
+        //     payable(receiver).transfer(userReceivable);
+        // }
+        // // Deleverage to 2x
+        // else if(operation == 2){
+        //     ( , , , uint swapAmount,) = abi.decode(params, (uint8, bytes, address, uint, uint));
+
+        //     // repay Weth on aave
+        //     WETH9.approve(address(aave), amounts[0]);
+        //     aave.repay(address(WETH9), amounts[0], 2, address(this));
+
+        //     // withdraw stEth from aave
+        //     aave.withdraw(asset, swapAmount, address(this));
+
+        //     // swap steth to Weth
+        //     uniswapV3Swap(SellInfo(
+        //         asset,
+        //         address(WETH9),
+        //         fee,
+        //         slippageAmt,
+        //         swapAmount
+        //     ));
+        // }
+        // // leverage 2x to 3x
+        // else if(operation == 3){
+        //     // swap Weth to supplyToken
+        //     uint returnAmount = uniswapV3Swap(SellInfo(
+        //         address(WETH9),
+        //         asset,
+        //         fee,
+        //         slippageAmt,
+        //         amounts[0]
+        //     ));
+
+        //     ERC20(asset).approve(address(aave), returnAmount);
+        //     aave.supply(asset, returnAmount, address(this), 0);
+
+        //     aave.borrow(address(WETH9), amounts[0] + premiums[0], 2, 0, address(this));
+        // }
+
+        uint amountOwing = amount + premium;
+        (VauitAction _action, bytes memory _actionParam) = abi.decode(params, (VauitAction, bytes));
+        RebalanceInput memory _rebalance = abi.decode(_actionParam, (RebalanceInput));
+        _rebalance.flAmountWithFee = amountOwing;
+        
+        rebalance(_rebalance.supplyAmount, _rebalance.flAmount, _rebalance.flAmountWithFee, _rebalance.slippagePercent);
+
+        IERC20(assetaddr).approve(aaveProvider.getPool(), amountOwing);
+        
+        // repay Aave
+        return true;
+    }
+
+    // /**
+	//  * @dev callFlashLoan
+	//  * @notice callFlashLoan function triggers aave flashloan in order to perform any opration like open position, Close position, deleverage, leverage.
+	//  * @param operation operation from enum operation
+	//  * @param amount amount
+	//  * @param _data1 data require for swap (dev : data is received from 1inch api by calling it offchain).
+	//  * @param _receiver receiver
+	//  */
+    function callFlashloan(address assetAddr, uint flashLoanAmount, bytes calldata param)public {
+        // uint supplyAmount;
+        // if(operation == 1){
+        //     // withdraw as per current leverage
+        //     (supplyAmount, amount) = convertToSupply(amount);
+        // }
+        // address[] memory assets = new address[](1);
+        // assets[0] = address(WETH9);
+
+        // uint[] memory amounts = new uint[](1);
+        // amounts[0] = amount;
+
+        // // 0 = no debt, 1 = stable, 2 = variable
+        // // 0 = pay all loaned
+        // uint[] memory modes = new uint[](1);
+        // modes[0] = 0;
+        
+        // (, SwapDescription memory desc,) = abi.decode(_data1[4:], (address, SwapDescription, bytes)); 
+
+        // bytes memory params = abi.encode(operation, _data1, _receiver, desc.amount, supplyAmount); // extra data to pass abi.encode(...)
+        // // uint16 referralCode = 0;
+
+
+        AaveInterface(aaveProvider.getPool()).flashLoanSimple(
+            address(this),
+            assetAddr,
+            flashLoanAmount,
+            param,
+            0
+        );
+    }
+
     
     /**
      * @dev Calculates the slippage amount given asset amount and slippage percentage.
@@ -146,28 +328,58 @@ contract WSTETHVault is FlashLoanSimpleReceiverBase, ERC20, IERC4626, Reentrancy
     }
 
     // /**
-	//  * @dev triggerFlashLoan
-	//  * @notice triggerFlashLoan function triggers aave flashloan in order to perform any opration like open position, Close position, deleverage, leverage.
-	//  * @param operation operation from enum operation
-	//  * @param amount amount
+	//  * @dev deleverage
+	//  * @notice Deleverage function can only be called by governance and it is used to deleverage the vault.
+	//  * @param amount  amount of borrwed Weth need to repay to deleverage the vault (this is given by function getborrowedAmountForDeleverage() )
 	//  * @param _data1 data require for swap (dev : data is received from 1inch api by calling it offchain).
-	//  * @param _receiver receiver
 	//  */
-    function triggerFlashLoan()private {
+    // function deleverage(uint amount, bytes calldata _data1) external nonReentrant onlyGovernance{
+    //     require(vaultsLeverage != Leverage.One, "can't go below 1x");
+        
+    //     triggerFlashLoan(uint8(Operation.Deleverage), amount, _data1, msg.sender);
+    //     if(vaultsLeverage == Leverage.Three) vaultsLeverage = Leverage.Two;
+    //     else vaultsLeverage = Leverage.One;
+    // }
 
-        address asset;
-        uint256 amount;
-        bytes memory params;
-        uint16 referralCode;
+    // /**
+	//  * @dev leverage
+	//  * @notice leverage function can only be called by governance and it is used to leverage the vault.
+	//  * @param amount amount of Weth need to borrow to leverage the vault  (this is given by function getVaultsActualBalance() )
+	//  * @param _data1 data require for swap (dev : data is received from 1inch api by calling it offchain).
+	//  */
+    // function leverage(uint amount, bytes calldata _data1) external nonReentrant onlyGovernance{
+    //     require(vaultsLeverage != Leverage.Three, "can't go above 3x");
+        
+    //     triggerFlashLoan(uint8(Operation.Leverage), amount, _data1, msg.sender);
+    //     if(vaultsLeverage == Leverage.Two) vaultsLeverage = Leverage.Three;
+    //     else vaultsLeverage = Leverage.Two;
+    // }
 
-        AaveInterface(aaveProvider.getPool()).flashLoanSimple(
-            address(this),
-            asset,
-            amount,
-            params,
-            0
-        );
-    }
+    // function _open1xLeverage(uint amount, bytes calldata data, address receiver)private {
+        
+    //     WETH9.approve(AGGREGATION_ROUTER_V5, amount);
+    //     uint returnAmount = swap(data);
+
+    //     uint shares;
+    //     require((shares = previewDeposit(returnAmount)) != 0, "ZERO_SHARES");
+    //     usersShareToBeMinted[receiver] = shares;
+
+    //     AaveInterface aave = AaveInterface(aaveProvider.getPool());
+    //     ERC20(asset).approve(address(aave), returnAmount);
+    //     aave.supply(asset, address(this), returnAmount);
+    // }
+
+    // function _close1xLeverage(uint amount, bytes calldata data, address _receiver) private{
+    //     // withdraw stEth from morpho
+    //     morpho.withdraw(pollToken, amount);
+
+    //     // swap stEth to Weth
+    //     ERC20(asset).approve(AGGREGATION_ROUTER_V5, amount);
+    //     uint returnAmount = swap(data);
+    //     WETH9.withdraw(returnAmount);
+    //     payable(_receiver).transfer(returnAmount);
+    // }
+
     
     function totalAssets() public view override returns (uint256){
         uint amount = getVaultsActualBalance();
@@ -248,6 +460,25 @@ contract WSTETHVault is FlashLoanSimpleReceiverBase, ERC20, IERC4626, Reentrancy
         amount = supplyBal - borrowBal;
     }
 
+    // // This function returns amount of borrwed Weth need to repay to deleverage the vault.
+    // function getborrowedAmountForDeleverage() public view returns(uint amount){
+    //     require(vaultsLeverage != Leverage.One, "can't go below 1x");
+    //     uint borrowBal = getDTokenAddr(address(WETH9), 2);
+    //     if(vaultsLeverage == Leverage.Two){
+    //         amount = borrowBal;
+    //     }
+    //     else{
+    //         uint vaultBal = getVaultsActualBalance();
+    //         amount = borrowBal - vaultBal;
+    //     }
+    // }
+
+    // function getborrowedAmountForleverage() public view returns(uint amount){
+    //     require(vaultsLeverage != Leverage.Three, "can't go above 3x");
+    //     uint vaultBal = getVaultsActualBalance();
+    //         amount = vaultBal;
+    // }
+
     /**
      * @notice `setGovernance()` should be called by the existing governance address prior to calling this function.
      */
@@ -267,7 +498,7 @@ contract WSTETHVault is FlashLoanSimpleReceiverBase, ERC20, IERC4626, Reentrancy
         emit GovernanceAccepted(governance);
     }
 
-    function getAssetPriceFromAave(address asset)public view returns(uint price, uint decimals){
+    function getAssetPriceFromAave(address asset)private view returns(uint price, uint decimals){
         address priceOracleAddress = aaveProvider.getPriceOracle();
         price = IAaveV3Oracle(priceOracleAddress).getAssetPrice(asset);
         decimals = IAaveV3Oracle(priceOracleAddress).BASE_CURRENCY_UNIT();
@@ -284,12 +515,13 @@ contract WSTETHVault is FlashLoanSimpleReceiverBase, ERC20, IERC4626, Reentrancy
         return balanceUSD;
     }
 
-    function getDebtAssetBalance()public view returns(uint balanceUSD){
+    function getDebtAssetBalance()public view returns(uint){
         (uint price, uint decimals) = getAssetPriceFromAave(address(WETH9));
         uint SupplyBalance = dTokenDebt.balanceOf(address(this));
         if(SupplyBalance <= 0) return 0;
 
         uint balanceUSD = (SupplyBalance * price) / decimals;
+        return balanceUSD;
     }
 
     function mint(uint256 shares, address receiver) external override nonReentrant returns (uint256 assets) {
--- smartContracts/test/Lock.js
+++ smartContracts/test/Lock.js
@@ -29,8 +29,53 @@ describe("WSTETHVault", function () {
     user = await accounts[0].getAddress();
   })
 
-  describe("deposit", function () {
-    it("Genesis deposit", async function () {
+  // describe("deposit", function () {
+  //   it("Genesis deposit", async function () {
+  //     let amount = ethers.utils.parseEther("0.1");
+
+  //     let wethContract = await hre.ethers.getContractAt("IWETH9", weth);
+  //     await wethContract.deposit({value: amount})
+
+  //     await wethContract.approve(vault.address, amount)
+      
+  //     let assetContract = await hre.ethers.getContractAt("IERC20", asset);
+  //     const balBefore = await assetContract.balanceOf(vault.address)
+  //     console.log("balBefore : ", balBefore.toString())
+          
+  //     const tx = await vault.connect(accounts[0]).deposit(amount.toString(), user, {gasLimit: 10000000});
+  //     await tx.wait();
+
+  //     const balAfter = await assetContract.balanceOf(vault.address)
+  //     console.log("balAfter : ", balAfter.toString())
+
+  //     const shares = await vault.balanceOf(user);
+  //     console.log("Share bal : ", shares.toString())
+
+  //     const totals = await vault.totalSupply()
+  //     const totalAssets = await vault.totalAssets()
+  //     const coll = await vault.getCollateralAssetBalance()
+  //     const price = await vault.getAssetPriceFromAave(asset)
+  //     console.log("console ", totals.toString(), totalAssets.toString(), coll.toString(), price[0].toString(), price[1].toString() )
+
+  //     const tx1 = await vault.connect(accounts[0]).withdraw(shares.toString(), user, user, {gasLimit: 10000000});
+  //     await tx1.wait();
+      
+  //     const sharesAfter = await vault.balanceOf(user);
+  //     console.log("Share bal after withdraw : ", sharesAfter.toString())
+
+      
+  //     const UserbalAfter = await wethContract.balanceOf(user)
+  //     console.log("UserbalAfter : ", UserbalAfter.toString())
+        
+  //     // expect(ethers.utils.formatEther(borrowedAmount[2].toString())).to.equal("2.0");
+  //     // expect(await stEthVault.balanceOf(user)).to.equal(amountWithoutFee);   
+
+  //   });
+
+  // });
+
+  describe("rebalance", function () {
+    it("rebalance", async function () {
       let amount = ethers.utils.parseEther("0.1");
 
       let wethContract = await hre.ethers.getContractAt("IWETH9", weth);
@@ -54,10 +99,36 @@ describe("WSTETHVault", function () {
       const totals = await vault.totalSupply()
       const totalAssets = await vault.totalAssets()
       const coll = await vault.getCollateralAssetBalance()
-      const price = await vault.getAssetPriceFromAave(asset)
-      console.log("console ", totals.toString(), totalAssets.toString(), coll.toString(), price[0].toString(), price[1].toString() )
+      // const price = await vault.getAssetPriceFromAave(asset)
+      console.log("console ", totals.toString(), totalAssets.toString(), coll.toString()) //, price[0].toString(), price[1].toString() )
 
-      const tx1 = await vault.connect(accounts[0]).withdraw(shares.toString(), user, user, {gasLimit: 10000000});
+      const abiCoder = new ethers.utils.AbiCoder();
+
+      const types = [
+        'uint256', // supplyAmount
+        'uint256', // flAmount
+        'uint256', // flAmountWithFee
+        'uint256'  // slippagePercent
+      ];
+
+      const paramTypes = [
+        'uint8', // vaultAction
+        'bytes' // actionParam
+      ];
+      
+      // Define the values
+      const supplyAmount = ethers.utils.parseEther("0.0848663"); //0.1697326
+      const flAmount = ethers.utils.parseEther("0.1");
+      const flAmountWithFee = 0;
+      const slippagePercent = 50000;
+      
+      // Encode the values
+      const encodedActionParams = abiCoder.encode(types, [supplyAmount, flAmount, flAmountWithFee, slippagePercent]);
+
+      const encodedParams = abiCoder.encode(paramTypes, [0, encodedActionParams]);
+    
+
+      const tx1 = await vault.connect(accounts[0]).callFlashloan(weth, flAmount, encodedParams, {gasLimit: 10000000});
       await tx1.wait();
       
       const sharesAfter = await vault.balanceOf(user);
@@ -66,6 +137,18 @@ describe("WSTETHVault", function () {
       
       const UserbalAfter = await wethContract.balanceOf(user)
       console.log("UserbalAfter : ", UserbalAfter.toString())
+
+      
+      let aToken = await hre.ethers.getContractAt("IAToken", "");
+      const abalance = await aToken.balanceOf(vault.address)
+      console.log("supplyAmount : ", supplyAmount.toString())
+      console.log("abalance : ", abalance.toString())
+      
+      let dToken = await hre.ethers.getContractAt("IDToken", "");
+      const dbalBefore = await dToken.balanceOf(vault.address)
+      console.log("dbalBefore : ", dbalBefore.toString())
+
+
         
       // expect(ethers.utils.formatEther(borrowedAmount[2].toString())).to.equal("2.0");
       // expect(await stEthVault.balanceOf(user)).to.equal(amountWithoutFee);   
